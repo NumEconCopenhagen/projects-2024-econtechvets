c029858cf71993049ef9df880416ced39e830336
RasmusDarling <hgs252@alumni.ku.dk>
2024-04-12 11:00:49 +0200

    update

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,16 +1,17 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
+# Path: projects-2024-econtechvets/dataproject/dataproject.py
+# Description: This file contains the functions used in the data project.
 
-    Args:
-        df (pd.DataFrame): pandas dataframe 
 
-    Returns:
-        df (pd.DataFrame): pandas dataframe
+#Function to calculate the share of players in each region/county. 
+def calculate_geographic_shares(idrakt):
 
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
+    idrakt_filtered = idrakt[idrakt['county'] != 'All Denmark']
+
+    # Group by geography and sum the player counts
+    total_players_by_region = idrakt_filtered.groupby('county')['value'].sum().reset_index()
+
+    # Calculate the share for each region
+    total_players = total_players_by_region['value'].sum()
+    total_players_by_region['share'] = total_players_by_region['value'] / total_players
+
+    return total_players_by_region
