67a3fc32a38e97493745568180bb394ec42aed3c
RasmusDarling <hgs252@alumni.ku.dk>
2024-04-12 12:56:28 +0200

    merge of datasets

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,9 +1,24 @@
 # Path: projects-2024-econtechvets/dataproject/dataproject.py
 # Description: This file contains the functions used in the data project.
+import pandas as pd
+import matplotlib.pyplot as plt
 
+def total_players_dk(df, filename='stacked_bar_chart.png'):
+    denmark_data = df[(df['region'] == 'All Denmark') & (df['sex'] != 'Sex, total')]
+    pivot_data = denmark_data.pivot(index='year', columns='sex', values='players')
+    ax = pivot_data.plot(kind='bar', stacked=True, figsize=(10, 7))
+    plt.title('Total Number of Players by Sex in All Denmark')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Players')
+    ax.legend(['Men', 'Women'], title='')
+    plt.tight_layout()
+    
+    # Save the figure
+    plt.savefig(filename)
+    plt.close()
 
 #Function to calculate the share of players in each region/county. 
-def calculate_geographic_shares(idrakt):
+def calculate_geographic_shares(df_merged):
 
     idrakt_filtered = idrakt[idrakt['county'] != 'All Denmark']
 
