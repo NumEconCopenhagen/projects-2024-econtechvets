98392b6c6c46f0d67f4ca01c8f88ca251943737a
Frederik Nellerod Nordentoft <hnw220@alumni.ku.dk>
2024-05-05 11:43:04 +0200

    Final adjustments

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,20 @@
 from scipy import optimize
+from types import SimpleNamespace
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
-
-    Args:
-        c (float): costs
-        alpha (float): parameter
-
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
-
-    """ 
+class modelprojectclass:
     
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
+    def __init__(self, T, G, M, P, a, b, c, d, e, f):
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
-    
-    return result
+        par = self.par = SimpleNamespace()
+
+        # Assign values to the parameters
+        par.T = T
+        par.G = G
+        par.M = M
+        par.P = P
+        par.a = a
+        par.b = b
+        par.c = c
+        par.d = d
+        par.e = e
+        par.f = f
